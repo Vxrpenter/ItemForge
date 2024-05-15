ff1eddd833f67c7e6d8c72431b5f6b8113446c9c
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2024-05-15 14:41:29 +0200

    DataStorage for UUID's

---
+++ src/main/java/dev/vxrp/itemforge/util/DataStorage/PersistentDataStorageUtil.java
@@ -0,0 +1,35 @@
+package dev.vxrp.itemforge.util.DataStorage;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.UUID;
+
+public class PersistentDataStorageUtil {
+    ItemStack itemStack;
+    ItemMeta itemMeta;
+    PersistentDataType<byte[], UUID> persistentDataType;
+
+    public PersistentDataStorageUtil(ItemStack itemStack) {
+        this.itemStack = itemStack;
+        this.itemMeta = itemStack.getItemMeta();
+        this.persistentDataType = new UUIDPersistentDataType();
+    }
+
+    public PersistentDataStorageUtil addUUID(NamespacedKey key, UUID uuid) {
+        PersistentDataType<byte[], UUID> persistentDataType = new UUIDPersistentDataType();
+        itemMeta.getPersistentDataContainer().set(key, persistentDataType, uuid);
+        itemStack.setItemMeta(itemMeta);
+        return this;
+    }
+
+    public UUID getStoredUUID(NamespacedKey key) {
+        return itemMeta.getPersistentDataContainer().get(key, persistentDataType);
+    }
+
+    public ItemStack build() {
+        return itemStack;
+    }
+}
---
+++ src/main/java/dev/vxrp/itemforge/util/DataStorage/UUIDPersistentDataType.java
@@ -0,0 +1,36 @@
+package dev.vxrp.itemforge.util.DataStorage;
+
+import org.bukkit.persistence.PersistentDataAdapterContext;
+import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+public class UUIDPersistentDataType implements PersistentDataType<byte[], UUID> {
+    @Override
+    public @NotNull Class<byte[]> getPrimitiveType() {
+        return byte[].class;
+    }
+
+    @Override
+    public @NotNull Class<UUID> getComplexType() {
+        return UUID.class;
+    }
+
+    @Override
+    public byte @NotNull [] toPrimitive(@NotNull UUID uuid, @NotNull PersistentDataAdapterContext context) {
+        ByteBuffer buffer = ByteBuffer.allocate(16);
+        buffer.putLong(uuid.getLeastSignificantBits());
+        buffer.putLong(uuid.getMostSignificantBits());
+        return buffer.array();
+    }
+
+    @Override
+    public @NotNull UUID fromPrimitive(byte @NotNull [] bytes, @NotNull PersistentDataAdapterContext context) {
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        long leastBits = buffer.getLong();
+        long mostBits = buffer.getLong();
+        return new UUID(mostBits, leastBits);
+    }
+}
